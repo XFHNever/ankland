8f4ab225166c089ade41f928f25d6cd962a23589
XFHNever <xfhnever@gmail.com>
2015-05-29 20:05:36 +0800

    Build structure

---
+++ ankland-admin/js/main.js
@@ -0,0 +1,13 @@
+(function(window, $, undefined){
+    var anklandService = {
+        attachEvents: function attachEvents(){
+
+        },
+        init: function(){
+            this.attachEvents();
+        }
+    };
+
+    anklandService.init();
+
+}(window || this, jQuery, undefined));
---
+++ ankland-front/js/good.js
@@ -0,0 +1,13 @@
+(function(window, $, undefined){
+    var goodService = {
+        attachEvents: function attachEvents(){
+
+        },
+        init: function(){
+            this.attachEvents();
+        }
+    };
+
+    goodService.init();
+
+}(window || this, jQuery, undefined));
