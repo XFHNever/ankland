d97df62888f830ac561ea3628dde6e0b719a30cf
XFHNever <xfhnever@gmail.com>
2015-06-08 00:30:10 +0800

    add modal for type.html

--- ankland-admin/js/type.js
+++ ankland-admin/js/type.js
@@ -4,6 +4,33 @@
             $('#typeTable').DataTable({
                 responsive: true
             });
+
+            $('button.create').on("click", function() {
+                var $createModel = $('#createModel');
+                $createModel.modal({
+                    backdrop: 'static'
+                });
+            });
+
+            $('tr')
+                .on("click", 'a.table-info', function() {
+                    var $infoModel = $('#infoModel');
+                    $infoModel.modal({
+                        backdrop: 'static'
+                    });
+                })
+                .on("click", 'a.table-edit', function() {
+                    var $editModel = $('#editModel');
+                    $editModel.modal({
+                        backdrop: 'static'
+                    });
+                })
+                .on("click", 'a.table-delete', function() {
+                    var $deleteModel = $('#deleteModel');
+                    $deleteModel.modal({
+                        backdrop: 'static'
+                    });
+                });
         },
         init: function(){
             this.attachEvents();
